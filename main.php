daa1e0a0c82c4551715b5fcc6a2ac0f83ab228ff
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-06-16 14:37:44 +0200

    Update main.php
    Added light/dark theme switcher functions

--- main.php
+++ main.php
@@ -14,6 +14,10 @@ if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
 $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && !empty($_SERVER['REMOTE_USER']) );
 $showSidebar = page_findnearest($conf['sidebar']) && ($ACT=='show');
 $sidebarElement = tpl_getConf('sidebarIsNav') ? 'nav' : 'aside';
+$themeMode = '';
+if (!empty($_COOKIE['theme'])){
+    $themeMode = $_COOKIE['theme'];
+}
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang'] ?>"
   lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>" class="no-js">
@@ -28,7 +32,7 @@ $sidebarElement = tpl_getConf('sidebarIsNav') ? 'nav' : 'aside';
 </head>
 
 
-<body id="kiwiki">
+<body id="kiwiki" class="<?php echo $themeMode; ?>">
     <?php /* the "dokuwiki__top" id is needed somewhere at the top, because that's where the "back to top" button/link links to */ ?>
     <?php /* tpl_classes() provides useful CSS classes; if you choose not to use it, the 'dokuwiki' class at least
              should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
