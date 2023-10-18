a29c802b3fb965b46afa96c69fe12570db72600d
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:51:34 +0200

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -7,5 +7,9 @@
 /*$meta['hideTools']        = array('onoff');
 $meta['sidebarIsNav']     = array('onoff');*/
 
-$meta['MainMenu'] = array('string');
+$meta['MainMenu']     = array('string');
 $meta['TocMaxHeight'] = array('numeric');
+$meta['GoBottomBtn']    = array('onoff');
+$meta['FullScreenBtn']    = array('onoff');
+$meta['ShowUserFooter']    = array('onoff');
+$meta['ShowACLFooter']    = array('onoff');
