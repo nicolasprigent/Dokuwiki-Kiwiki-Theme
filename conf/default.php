4836e5606d5594a626e30df0f89fac4ecf7ecdbd
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:51:22 +0200

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -6,3 +6,7 @@
 
 $conf['MainMenu'] = 'mainmenu';
 $conf['TocMaxHeight'] = '300';
+$conf['GoBottomBtn']    = 0;
+$conf['FullScreenBtn']    = 1;
+$conf['ShowUserFooter']    = 0;
+$conf['ShowACLFooter']    = 0;
