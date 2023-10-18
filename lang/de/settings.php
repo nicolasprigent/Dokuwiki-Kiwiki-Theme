e57434abb52d21c293a2cdfaa0ca72b11d836359
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:55:09 +0200

    Update settings.php

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -6,3 +6,6 @@
 
 $lang['MainMenu'] = 'Menü-Bezeichner';
 $lang['TocMaxHeight'] = 'Max. Höhe des Inhaltsverzeichnisses';
+$lang['GoBottomBtn'] = 'Zur unteren Taste gehen';
+$lang['ShowUserFooter']    = 'Angemeldeten Benutzer im Footer anzeigen';
+$lang['ShowACLFooter']    = 'Gruppenberechtigungen (ACL) im Footer anzeigen';
