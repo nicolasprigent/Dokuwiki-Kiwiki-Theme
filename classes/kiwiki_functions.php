477069bfbca8c3e7dab7af3c68a319d26bea032a
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 17:26:23 +0200

    Update kiwiki_functions.php

--- classes/kiwiki_functions.php
+++ classes/kiwiki_functions.php
@@ -26,45 +26,48 @@ class Kiwiki_Functions {
             .'</button></a>';
     }
 
+    /**
+     * Custom ACL Informations in footer
+     * Original code from Chris75
+     * https://forum.dokuwiki.org/d/21636-acl-deny-a-user-group-to-a-namespace/13
+     */
 
-    public static function tpl_aclinfo() {
-        global $ID, $AUTH_ACL;
-     
-        if (auth_quickaclcheck($ID) == 0) 
+     public function tpl_aclinfo() {
+        global $ID, $AUTH_ACL, $INFO;
+        
+        if ((auth_quickaclcheck($ID) == 0) ||(!$INFO['editable']))
             return; // no rights to view, no rights to get this info
-     
+        
         $page_acls = array();
         $namespaces = array();
-     
+        
         $ns = getNS($ID);
         while ($ns) {
-           array_unshift($namespaces,$ns . ':*');
-           $ns = getNS($ns);  
+        array_unshift($namespaces,$ns . ':*');
+        $ns = getNS($ns);  
         }
         array_unshift($namespaces,'*'); // root
-     
+        
         $namespaces[] = $ID;
-     
+        
         // check matches
         foreach ($namespaces as $level) {
             $matches = preg_grep('/^'.preg_quote($level,'/').'\s+\S+\s+\d+\s*$/',$AUTH_ACL);
             $this_acls = array();
             foreach($matches as $match){
-              $match = preg_replace('/#.*$/','',$match); //ignore comments
-              $acl   = preg_split('/\s+/',$match);
-              $this_acls[urldecode($acl[1])] = $acl[2];
-              if ($acl[1] == "@ALL")  // overwrites stuff from upper level
+            $match = preg_replace('/#.*$/','',$match); //ignore comments
+            $acl   = preg_split('/\s+/',$match);
+            $this_acls[urldecode($acl[1])] = $acl[2];
+            if ($acl[1] == "@ALL")  // overwrites stuff from upper level
                 $page_acls = array();
             }
             $page_acls =  array_merge($page_acls,$this_acls);
         }
-     
-        // check if visible to everyone:
-        if (($page_acls['@ALL'] !== false) && $page_acls['@ALL'] > 0) 
-          return; // page is visible to everyone
         
-        $return = "";
-
+        // check if visible to everyone:
+        /*if (($page_acls['@ALL'] !== false) && $page_acls['@ALL'] > 0) 
+        return; // page is visible to everyone*/
+        
         $list = array();	
         foreach ($page_acls as $user => $right) {
             if ($right > 0 && $user != "@admin" ) // admins can see everything
@@ -72,24 +75,22 @@ class Kiwiki_Functions {
         }
         if (count($list)) {
             sort($list);
-            $return = "(page visible to: ";
-            $return .= join(', ',$list);
-            $return .= ")";
+            print "(page visible to: ";
+            print join(', ',$list);
+            print ")";
         }
-     
+        
         // Uncomment this, if you want to display users/groups who cannot access this page, too:
-        $list = array();	
+        $list = array();
         foreach ($page_acls as $user => $right) {
             if ($right == 0)
                 array_push($list,$user);
         }
         if (count($list)) {
             sort($list);
-            $return .= "(page hidden to: ";
-            $return .= join(', ',$list);
-            $return .= ")";
+            print "(page hidden to: ";
+            print join(', ',$list);
+            print ")";
         }
-
-        return $return;
     }
 }
