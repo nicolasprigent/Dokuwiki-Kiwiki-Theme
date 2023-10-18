5df132cb466fa5e66ffccbfab3a9a29bbda3bcd9
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:55:29 +0200

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -6,3 +6,6 @@
 
 $lang['MainMenu'] = 'Menu identifier';
 $lang['TocMaxHeight'] = 'Max. table of contents height';
+$lang['GoBottomBtn'] = 'Go to bottom button';
+$lang['ShowUserFooter']    = 'Display logged-in user in footer';
+$lang['ShowACLFooter']    = 'Display group permissions (ACL) in footer';
