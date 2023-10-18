2503341084603c3ded05809fc1d2b63d9c0cf436
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:51:03 +0200

    Update and rename kiwiki_go_top.php to kiwiki_go.php
    new go bottom button

---
+++ classes/kiwiki_go.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace dokuwiki\Menu;
+
+/**
+ * Class KiwikiGo
+ *
+ * Go to top button
+ */
+class KiwikiGo extends AbstractMenu {
+
+    public $types = array(
+        'Top',
+        'Bottom'
+    );
+
+}
