51a791654c54bb73cc63a4efb11412449397ddce
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-10-18 15:56:22 +0200

    Update footer.php

--- partial/footer.php
+++ partial/footer.php
@@ -1,5 +1,18 @@
-<footer id="dokuwiki__footer">        
-    <div class="doc"><?php tpl_pageinfo() ?></div>
+<footer id="dokuwiki__footer">
+    <div class="meta">
+        
+        <?php if (tpl_getConf('ShowUserFooter')){
+        echo '<div class="user">';
+        tpl_userinfo();
+        echo '</div>';
+        }
+        ?>
+
+        <?php if (tpl_getConf('ShowACLFooter'))
+        echo '<div class="acl">' . Kiwiki_Functions::tpl_aclinfo() . '</div>'?>
+        
+        <div class="doc"><?php tpl_pageinfo() ?></div>
+    </div>
     <?php tpl_license('button') ?>
     <?php tpl_includeFile('footer.html') ?>
 </footer>
