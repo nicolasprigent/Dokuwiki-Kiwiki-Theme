85a7f8d06a2368efdda9ae73a7a5b9ab73c4697c
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2023-11-22 23:31:57 +0100

    Update kiwiki_functions.php
    check permissions of current user to get the edit button for editors and not just admins

--- classes/kiwiki_functions.php
+++ classes/kiwiki_functions.php
@@ -5,7 +5,7 @@ class Kiwiki_Functions {
     public static function _edit_icon($what, $usergroup, $action){
         $output = "";
         if (isset($usergroup)){
-            if (in_array('admin',$usergroup) && $action == 'show'){                        
+            if ((auth_quickaclcheck($USERINFO['id']) > AUTH_READ) && $action == 'show'){                     
                 $output = '<a class="edit-this" href="doku.php?id=' . $what . '&do=edit">' . inlineSVG(KIWIKI_IMAGES_PATH . 'edit.svg') .'</a>';
             }
         }
