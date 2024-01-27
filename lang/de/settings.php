189bfe5be4af929f14e2cd666267ae383c8e3950
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2024-01-27 16:21:37 +0100

    Update settings.php

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -5,6 +5,7 @@
  */
 
 $lang['MainMenu'] = 'Menü-Bezeichner';
+$lang['MenuMaxHeight'] = 'Max. Höhe des Menü';
 $lang['TocMaxHeight'] = 'Max. Höhe des Inhaltsverzeichnisses';
 $lang['GoBottomBtn'] = 'Zur unteren Taste gehen';
 $lang['ShowUserFooter']    = 'Angemeldeten Benutzer im Footer anzeigen';
