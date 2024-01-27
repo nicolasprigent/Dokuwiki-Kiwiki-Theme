4f76ef8ae824a11475ae2e37d569b4e6faa98ec9
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2024-01-27 16:21:23 +0100

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,6 +5,7 @@
  */
 
 $lang['MainMenu'] = 'Menu identifier';
+$lang['MenuMaxHeight'] = 'Max. menu height';
 $lang['TocMaxHeight'] = 'Max. table of contents height';
 $lang['GoBottomBtn'] = 'Go to bottom button';
 $lang['ShowUserFooter']    = 'Display logged-in user in footer';
