256b76006e70e358110796e2d0f61ae0765c80f0
Nicolas Prigent <66468412+nicolasprigent@users.noreply.github.com>
2024-09-16 21:47:05 +0200

    Update default.php
    Added customizable content width

--- conf/default.php
+++ conf/default.php
@@ -8,6 +8,7 @@ $conf['MainMenu'] = 'mainmenu';
 $conf['MenuMaxHeight'] = '300';
 $conf['TocMaxHeight'] = '300';
 $conf['GoBottomBtn']    = 0;
+$conf['ContentMaxWidth']    = '1300px';
 $conf['FullScreenBtn']    = 1;
 $conf['ShowUserFooter']    = 0;
 $conf['ShowACLFooter']    = 0;
