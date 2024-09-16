4e090d40b8b4cbf72c1657467806994b3e61901d
Nicolas Prigent <66468412+nicolasprigent@users.noreply.github.com>
2024-09-16 21:47:37 +0200

    Update metadata.php
    Added customizable content width

--- conf/metadata.php
+++ conf/metadata.php
@@ -8,6 +8,7 @@ $meta['MainMenu']     = array('string');
 $meta['MenuMaxHeight'] = array('numeric');
 $meta['TocMaxHeight'] = array('numeric');
 $meta['GoBottomBtn']    = array('onoff');
+$meta['ContentMaxWidth'] = array('string');
 $meta['FullScreenBtn']    = array('onoff');
 $meta['ShowUserFooter']    = array('onoff');
 $meta['ShowACLFooter']    = array('onoff');
