6abfdb423815eb09c05352059755199b3a221e45
simon-eller <63782380+simon-eller@users.noreply.github.com>
2025-04-24 14:47:56 +0200

    Added informal variant of German language according to https://translate.dokuwiki.org/dokuwiki

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * German (informal) language file for config
+ *
+ */
+
+$lang['MainMenu'] = 'Menü-Bezeichner';
+$lang['MenuMaxHeight'] = 'Max. Höhe des Menü';
+$lang['TocMaxHeight'] = 'Max. Höhe des Inhaltsverzeichnisses';
+$lang['GoBottomBtn'] = 'Zur unteren Taste gehen';
+$lang['ShowUserFooter']    = 'Angemeldeten Benutzer im Footer anzeigen';
+$lang['ShowACLFooter']    = 'Gruppenberechtigungen (ACL) im Footer anzeigen';
+$lang['FullScreenBtn']    = 'Vollbild-Taste';
