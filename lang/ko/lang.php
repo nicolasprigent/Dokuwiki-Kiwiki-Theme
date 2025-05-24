d1a1df22a6862df7409f2709328b9b2dc62e2b53
Curic <137182263+curicbangkok@users.noreply.github.com>
2025-05-24 12:55:43 +0700

    Add initial Korean translation support

---
+++ lang/ko/lang.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 템플릿을 위한 한국어 파일
+ *
+ */
+
+$lang['__existing__']      = '페이지가 존재하는 링크 색상';
+$lang['__missing__']       = '페이지가 존재하지 않는 링크 색상';
+$lang['__site_width__']    = '사이트 너비';
+$lang['__sidebar_width__'] = '사이드바 너비';
+$lang['search'] = '검색';
+$lang['Menu']        = '메뉴';
+$lang['edit-menu']   = '메뉴 편집';
+$lang['Back to homepage']   = '홈페이지로 돌아가기';
+$lang['Visible to'] = "보여지는 페이지 목록: " ; 
+$lang['Hidden to'] = "숨겨진 페이지 목록 : " ; 
+$lang['User is in group'] = "당신은 다음 그룹에 속해 있습니다:" ; 
+$lang['btn_bottom'] = "아래로 내리기" ; 
+
+$lang['__kiwiki_body_bg_color__'] = "배경 색상";
+$lang['__kiwiki_header_bg_color__'] = "Header 배경 색상";
+$lang['__kiwiki_text_color__'] = "문자 색상";
+$lang['__kiwiki_main_color__'] = "컨텐츠 배경 색상" ;
+$lang['__kiwiki_ui_color__'] = "UI 색상" ;
+$lang['__kiwiki_ui_100_color__'] = "UI 색상 100" ;
+$lang['__kiwiki_ui_200_color__'] = "UI 색상 200";
+$lang['__kiwiki_ui_300_color__'] = "UI 색상 300";
+$lang['__kiwiki_ui_400_color__'] = "UI 색상 400" ; 
+$lang['__kiwiki_ui_500_color__'] = "UI 색상 500" ; 
+$lang['__kiwiki_ui_600_color__'] = "UI 색상 600" ; 
+$lang['__kiwiki_ui_700_color__'] = "UI 색상 700" ;
+$lang['__kiwiki_ui_800_color__'] = "UI 색상 800" ; 
+$lang['__kiwiki_ui_900_color__'] = "UI 색상 900" ; 
+$lang['__kiwiki_btn_bg_color__'] = "버튼 배경 색상" ; 
+$lang['__kiwiki_btn_text_color__'] = "버튼 문자 색상" ; 
+$lang['__kiwiki_btn_bg_hover_color__'] = "마우스를 올렸을 때 버튼 배경 색상" ; 
+$lang['__kiwiki_btn_hover_text_color__'] = "마우스를 올렸을 때 버튼 문자 색상" ; 
+$lang['__kiwiki_color_btn_border_color__'] = "버튼 경계선 색상" ; 
+
+$lang['__kiwiki_dark_body_bg_color__'] = "배경 색상" ; 
+$lang['__kiwiki_dark_header_bg_color__'] = "Header 배경 색상";
+$lang['__kiwiki_dark_text_color__'] = "문자 색상" ; 
+$lang['__kiwiki_dark_main_color__'] = "컨텐츠 배경 색상" ; 
+$lang['__kiwiki_dark_ui_color__'] = "UI Color" ; 
+$lang['__kiwiki_dark_ui_100_color__'] = "UI Color 100" ; 
+$lang['__kiwiki_dark_ui_200_color__'] = "UI Color 200" ; 
+$lang['__kiwiki_dark_ui_300_color__'] = "UI Color 300" ; 
+$lang['__kiwiki_dark_ui_400_color__'] = "UI Color 400" ; 
+$lang['__kiwiki_dark_ui_500_color__'] = "UI Color 500" ; 
+$lang['__kiwiki_dark_ui_600_color__'] = "UI Color 600" ; 
+$lang['__kiwiki_dark_ui_700_color__'] = "UI Color 700" ; 
+$lang['__kiwiki_dark_ui_800_color__'] = "UI Color 800" ; 
+$lang['__kiwiki_dark_ui_900_color__'] = "UI Color 900" ; 
+$lang['__kiwiki_dark_btn_bg_color__'] = "버튼 배경 색상" ; 
+$lang['__kiwiki_dark_btn_text_color__'] = "버튼 문자 색상" ; 
+$lang['__kiwiki_dark_btn_bg_hover_color__'] = "마우스를 올렸을 때 버튼 배경 색상" ; 
+$lang['__kiwiki_dark_btn_hover_text_color__'] = "마우스를 올렸을 때 버튼 문자 색상" ; 
+$lang['__kiwiki_dark_color_btn_border_color__'] = "버튼 경계선 색상" ; 
---
+++ lang/ko/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 환경 설정을 위한 한국어 파일
+ *
+ */
+
+$lang['MainMenu'] = '메뉴 식별 문서';
+$lang['MenuMaxHeight'] = '최대 메뉴 높이';
+$lang['TocMaxHeight'] = '최대 컨텐츠 테이블 높이';
+$lang['GoBottomBtn'] = '아래로 내리기 버튼';
+$lang['ShowUserFooter']    = '로그인한 사용자를 footer에 보여주기';
+$lang['ShowACLFooter']    = '그룹 권한 (ACL)을 footer에 보여주기';
+$lang['FullScreenBtn']    = '전체화면 버튼';
+$lang['ContentMaxWidth']    = '컨텐츠 최대 너비 (유닛이 필요한 경우)';
+$lang['DefaultTheme']    = '기본 테마 색상';
+$lang['ForceTheme']    = '기본 테마 색상 강제 적용 (header에서 전환 버튼을 끔)';
