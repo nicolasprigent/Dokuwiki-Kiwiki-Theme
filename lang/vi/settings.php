2dd76ccd464df8935838d53028cbc6ff332a372b
Dzung Do <dungdo@osd.vn>
2025-07-23 02:38:57 +0700

    Create settings.php

---
+++ lang/vi/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Vietnamese language file for config
+ *
+ */
+
+$lang['MainMenu'] = 'Định dạng trình đơn';
+$lang['SidebarMaxHeight'] = 'Chiều cao sidebar tối đa';
+$lang['SidebarRight'] = 'Sidebar bên phải?';
+$lang['TocMaxHeight'] = 'Chiều cao mục lục tối đa';
+$lang['TocRight'] = 'Mục lục bên phải?';
+$lang['GoBottomBtn'] = 'Nút đi xuống dưới';
+$lang['ShowUserFooter']    = 'Hiển thị người dùng đăng nhập dưới chan trang';
+$lang['ShowACLFooter']    = 'Hiển thị quyền nhóm (ACL) dưới chân trang';
+$lang['FullScreenBtn']    = 'Nút toàn màn hình';
+$lang['ContentMaxWidth']    = 'Độ rộng nội dung tối đa (gồm đơn vị tính)';
+$lang['DefaultTheme']    = 'Màu chủ đề mặc định';
+$lang['ForceTheme']    = 'Bắt buộc dùng màu mặc định (sẽ tắt nút chuyển chủ đề tại đầu trang)';
