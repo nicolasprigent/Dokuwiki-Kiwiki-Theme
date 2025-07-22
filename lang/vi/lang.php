5c664222e9cd10ec14d88cf3c555a0b44f4cbdd4
Dzung Do <dungdo@osd.vn>
2025-07-23 02:34:47 +0700

    Create lang.php

---
+++ lang/vi/lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Vietnamese language file for template
+ *
+ */
+
+$lang['__existing__']      = 'Màu liên kết trang tồn tại';
+$lang['__missing__']       = 'Màu liên kết trang không tồn tại';
+$lang['__site_width__']    = 'Độ rộng site';
+$lang['__sidebar_width__'] = 'Độ rộng sidebar';
+$lang['search'] = 'Tìm kiếm';
+$lang['Menu']        = 'Trình đơn';
+$lang['edit-menu']   = 'Sửa trình đơn';
+$lang['Back to homepage']   = 'Về trang chủ';
+$lang['Visible to'] = "Trang hiển thị với: " ; 
+$lang['Hidden to'] = "Trang ẩn với: " ; 
+$lang['User is in group'] = "Bạn thuộc các nhóm sau:" ; 
+$lang['btn_bottom'] = "Xuống dưới cùng" ; 
+$lang['full-screen'] = "Toàn màn hình" ; 
+$lang['theme-mode'] = "Chế độ ngày/đêm" ; 
+$lang['user-tools'] = "Công cụ người dùng" ; 
+$lang['page-tools'] = "Công cụ trang" ; 
+$lang['main-menu'] = "Trình đơn chính" ; 
+$lang['close'] = "Đóng" ; 
+
+$lang['__kiwiki_body_bg_color__'] = "Màu nền";
+$lang['__kiwiki_header_bg_color__'] = "Màu nền header";
+$lang['__kiwiki_text_color__'] = "Màu chữ";
+$lang['__kiwiki_main_color__'] = "Màu nền nội dung" ;
+$lang['__kiwiki_ui_color__'] = "Màu UI" ;
+$lang['__kiwiki_ui_100_color__'] = "UI Color 100" ;
+$lang['__kiwiki_ui_200_color__'] = "UI Color 200";
+$lang['__kiwiki_ui_300_color__'] = "UI Color 300";
+$lang['__kiwiki_ui_400_color__'] = "UI Color 400" ; 
+$lang['__kiwiki_ui_500_color__'] = "UI Color 500" ; 
+$lang['__kiwiki_ui_600_color__'] = "UI Color 600" ; 
+$lang['__kiwiki_ui_700_color__'] = "UI Color 700" ;
+$lang['__kiwiki_ui_800_color__'] = "UI Color 800" ; 
+$lang['__kiwiki_ui_900_color__'] = "UI Color 900" ; 
+$lang['__kiwiki_btn_bg_color__'] = "Màu nền nút" ; 
+$lang['__kiwiki_btn_text_color__'] = "Màu chữ nút" ; 
+$lang['__kiwiki_btn_bg_hover_color__'] = "Màu nền nút khi hover"; 
+$lang['__kiwiki_btn_hover_text_color__'] = "Màu chữ nút khi hover" ; 
+$lang['__kiwiki_color_btn_border_color__'] = "Màu viền nút" ; 
+
+$lang['__kiwiki_dark_body_bg_color__'] = "Màu nền" ; 
+$lang['__kiwiki_dark_header_bg_color__'] = "Màu nền đầu trang";
+$lang['__kiwiki_dark_text_color__'] = "Màu chữ" ; 
+$lang['__kiwiki_dark_main_color__'] = "Màu nền nội dung" ; 
+$lang['__kiwiki_dark_ui_color__'] = "UI Color" ; 
+$lang['__kiwiki_dark_ui_100_color__'] = "UI Color 100" ; 
+$lang['__kiwiki_dark_ui_200_color__'] = "UI Color 200" ; 
+$lang['__kiwiki_dark_ui_300_color__'] = "UI Color 300" ; 
+$lang['__kiwiki_dark_ui_400_color__'] = "UI Color 400" ; 
+$lang['__kiwiki_dark_ui_500_color__'] = "UI Color 500" ; 
+$lang['__kiwiki_dark_ui_600_color__'] = "UI Color 600" ; 
+$lang['__kiwiki_dark_ui_700_color__'] = "UI Color 700" ; 
+$lang['__kiwiki_dark_ui_800_color__'] = "UI Color 800" ; 
+$lang['__kiwiki_dark_ui_900_color__'] = "UI Color 900" ; 
+$lang['__kiwiki_dark_btn_bg_color__'] = "Màu nền nút" ; 
+$lang['__kiwiki_dark_btn_text_color__'] = "Màu chữ nút" ; 
+$lang['__kiwiki_dark_btn_bg_hover_color__'] = "Màu nền nút khi hover" ; 
+$lang['__kiwiki_dark_btn_hover_text_color__'] = "Màu nền chữ khi hover" ; 
+$lang['__kiwiki_dark_color_btn_border_color__'] = "Màu viền nút" ; 
